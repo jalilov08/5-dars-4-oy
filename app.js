009b345534db663cd9f8875c2507e3375fbd2af2
jalilov08 <jalilovnozimjon054@gmail.com>
2024-04-30 22:41:12 +0500

    Homework

---
+++ app.js
@@ -0,0 +1,219 @@
+// Vazifa
+//  1--
+// function calculate(a, b) {
+//   return a + b;
+// }
+// console.log(calculate(3, 6));
+
+
+// 2--
+// function toSecunds(minutes) {
+//   return minutes * 60;
+// }
+// console.log(toSecunds(7) + " sekund");
+
+
+// 3--
+// function nextInt(int) {
+//   return int + 1;
+// }
+// console.log(nextInt(7));
+
+
+// 4--
+// function triangleArea(a, b) {
+//   return (a * b) / 2;
+// }
+// console.log(triangleArea(3, 6));
+
+
+// 5--
+// function ageToDays(age) {
+//   return age * 365;
+// }
+// console.log(ageToDays(27));
+
+
+// 6--
+// function calculate(num) {
+//   return num  3;
+// }
+// console.log(calculate(5));
+
+
+// 7--
+// function firstNum(array) {
+//   return array[0];
+// }
+// let array = [1, 2, 3];
+// console.log(firstNum(array));
+// console.log(firstNum(["a", "b", "c"]));
+
+
+// 8--
+// function calculate(a, b) {
+//   return a * b;
+// }
+// console.log(calculate(3, 6));
+
+
+// 9--
+// function toSecunds(h) {
+//   return h * 3600;
+// }
+// console.log(toSecunds(24) + " sekund");
+
+
+// 10--
+// function calculate(a, b) {
+//   return a + b - 1;
+// }
+// console.log(calculate(3, 6));
+
+
+// 11--
+// function calculate(a, b) {
+//   return a % b;
+// }
+// console.log(calculate(1, 3));
+// console.log(calculate(-9, 45));
+// console.log(calculate(3, 4));
+
+
+// 12--
+// let areaOfRectangle = (a, b) => {
+//   return a * b;
+// };
+// console.log(areaOfRectangle(3, 4));
+
+
+// 13--
+// const addString = (a) => {
+//   return `Someting ${a}`;
+// };
+// console.log(addString("Teacher"));
+
+
+// 14--
+// const sqr = (n) => {
+//   return n  2;
+// };
+// console.log(sqr(5));
+
+
+// 15--
+// function zero(n) {
+//   if (n <= 0) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// zero(-8);
+
+
+// 16--
+// function calculate(n) {
+//   return (n - 2) * 180;
+// }
+// console.log(calculate(4));
+
+
+// 17--
+// function calculate(x, y) {
+//   return x * 2 + y * 3;
+// }
+// console.log(calculate(4, 2));
+
+
+// 18--
+// const addString = (a) => {
+//   return ${a}Edabit;
+// };
+// console.log(addString("Teacher"));
+
+
+// 19--
+// const biggerThanHundred = function (a, b) {
+//   if (a + b < 100) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// };
+// biggerThanHundred(34, 45);
+// biggerThanHundred(34, 75);
+
+
+// 20--
+// let newArray = [];
+// function array(n) {
+//   for (i = 1; i <= n; i++) {
+//     newArray.push(i);
+//   }
+// }
+// array(20);
+// console.log(newArray);
+
+
+// 21--
+// function countLegs(tovuq, qoy, mol) {
+//   return tovuq * 2 + qoy * 4 + mol * 4;
+// }
+// console.log(countLegs(2, 3, 5));
+
+
+//22--
+// function bool(a, b) {
+//   if (a == true && b == false) {
+//     console.log(false);
+//   } else if (a == true && b == true) {
+//     console.log(true);
+//   } else if (a == false && b == true) {
+//     console.log(false);
+//   } else if (a == false && b == false) {
+//     console.log(false);
+//   }
+// }
+// bool(true, false);
+// bool(true, true);
+
+
+// 23--
+// function isEqual(a, b) {
+//   if (a === b) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// isEqual(4, 4);
+// isEqual(4, 8);
+
+
+// 24--
+// function points(win, draw, lost) {
+//   return win * 3 + draw * 1;
+// }
+// console.log(points(6, 2, 3));
+
+// 25--
+// function secunds(h, m) {
+//   return h * 3600 + m * 60;
+// }
+// console.log(secunds(1, 2));
+
+
+// 26--
+// function fun(n) {
+//   if (n === 7) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// fun(4);
+// fun(7);
+// fun(9);
+
+
